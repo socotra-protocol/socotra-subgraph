502d4475a79ce49365f2ae1c3fea7da49453dc58
Poomrapee Chayvimol <poomch@Poomrapees-MacBook-Pro-2.local>
2022-05-22 13:50:53 +0700

    manager mapping handle functions

--- src/manager.ts
+++ src/manager.ts
@@ -3,7 +3,20 @@ import {
   SocotraBranchManager,
   ProxyRegistered,
   UpdateSnapshot,
+  DelegateSpace,
+  RegisterMember,
+  ClaimToken,
+  RequestPayout,
+  WithdrawPayout,
+  IssuePayout,
 } from "../generated/SocotraBranchManager/SocotraBranchManager";
 import { Branch } from "../generated/schema";
 
 export function handleProxyRegistered(event: ProxyRegistered): void {}
+export function handleUpdateSnapshot(event: UpdateSnapshot): void {}
+export function handleDelegateSpace(event: DelegateSpace): void {}
+export function handleRegisterMember(event: RegisterMember): void {}
+export function handleClaimToken(event: ClaimToken): void {}
+export function handleRequestPayout(event: RequestPayout): void {}
+export function handleWithdrawPayout(event: WithdrawPayout): void {}
+export function handleIssuePayout(event: IssuePayout): void {}
